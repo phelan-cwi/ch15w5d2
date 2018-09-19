96355e1892e4dc7837e4800c89ff68ab7ab67a29
Phelan <phelanjaramillo@mycwi.cc>
2018-09-19 10:02:56 -0600

    first commit

---
+++ index2.php
@@ -0,0 +1,33 @@
+<?php
+
+    $visitor_name = '';
+    $visitor_email = 'adam@thevoice.com';
+    $visitor_msg = 'Great job!';
+    /* echo "Fields: " . $visitor_name . $visitor_email . $visitor_msg;  */
+    
+    // Validate inputs
+    if ($visitor_name == null || $visitor_email == null ||
+        $visitor_msg == null) {
+        $error = "Invalid input data. Check all fields and try again.";
+        /* include('error.php'); */
+        echo "Form Data Error: " . $error; 
+        exit();
+        } else {
+            $dsn = 'mysql:host=localhost;dbname=evajones';
+            $username = 'root';
+            $password = 'Pa$$w0rd';
+            
+            try {           
+                $db = 
+                    new PDO($dsn, $username, $password);
+                // other statements
+            } catch (PDOException $e) {
+                echo 'PDOException: ' . $e->getMessage();
+                exit;
+            }
+            echo 'No errors.';
+        }
+
+
+
+
